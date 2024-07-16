e05010c5f07eb6736f949f5c1503ce2cf2834d09
saad <saad@globex>
2024-07-16 17:49:03 +0400

    changed landy to shayam traders

--- src/components/privacy-policy/PrivacyPolicy.tsx
+++ src/components/privacy-policy/PrivacyPolicy.tsx
@@ -2,30 +2,15 @@ import { Row, Col } from "antd";
 import { Fade } from "react-awesome-reveal";
 import { withTranslation } from "react-i18next";
 
-import { ContentBlockProps } from "../ContentBlock/types";
-import { Button } from "../../common/Button";
-import { SvgIcon } from "../../common/SvgIcon";
 import {
   ContentSection,
   Content,
-  ContentWrapper,
   ServiceWrapper,
-  MinTitle,
-  MinPara,
-  StyledRow,
-  ButtonWrapper,
   PrivacyPolicyWrapper,
   PrivacySectionHeadings,
 } from "../ContentBlock/styles";
 
 const PrivacyPolicy = () => {
-  const scrollTo = (id: string) => {
-    const element = document.getElementById(id) as HTMLDivElement;
-    element.scrollIntoView({
-      behavior: "smooth",
-    });
-  };
-
   return (
     <ContentSection>
       <Fade direction={"left"} triggerOnce>
@@ -34,62 +19,63 @@ const PrivacyPolicy = () => {
             <h6>{"Privacy Policy"}</h6>
             <Content>
               These terms and conditions outline the rules and regulations for
-              the use of TradeXlive Website, located at https://www.tradex.live
-              By accessing this website we assume you accept these terms and
-              conditions. Do not continue to use https://www.tradex.live if you
-              do not agree to take all of the terms and conditions stated on
-              this page.The following terminology applies to these Terms and
-              Conditions, Privacy Statement and Disclaimer Notice and all
-              Agreements: “Client”, “You” and “Your” refers to you, the person
-              log on this website and compliant to the Company’s terms and
-              conditions. “The Company”, “Ourselves”, “We”, “Our” and “Us”,
-              refers to our Company. “Party”, “Parties”, or “Us”, refers to both
-              the Client and ourselves. All terms refer to the offer, acceptance
-              and consideration of payment necessary to undertake the process of
-              our assistance to the Client in the most appropriate manner for
-              the express purpose of meeting the Client’s needs in respect of
-              provision of the Company’s stated services, in accordance with and
-              subject to, prevailing law. Any use of the above terminology or
-              other words in the singular, plural, capitalization and/or he/she
-              or they, are taken as interchangeable and therefore as referring
-              to the same.
+              the use of Shyam traders Website, located at
+              https://www.shyamtraders.net By accessing this website we assume
+              you accept these terms and conditions. Do not continue to use
+              https://www.shyamtraders.net if you do not agree to take all of
+              the terms and conditions stated on this page.The following
+              terminology applies to these Terms and Conditions, Privacy
+              Statement and Disclaimer Notice and all Agreements: “Client”,
+              “You” and “Your” refers to you, the person log on this website and
+              compliant to the Company’s terms and conditions. “The Company”,
+              “Ourselves”, “We”, “Our” and “Us”, refers to our Company. “Party”,
+              “Parties”, or “Us”, refers to both the Client and ourselves. All
+              terms refer to the offer, acceptance and consideration of payment
+              necessary to undertake the process of our assistance to the Client
+              in the most appropriate manner for the express purpose of meeting
+              the Client’s needs in respect of provision of the Company’s stated
+              services, in accordance with and subject to, prevailing law. Any
+              use of the above terminology or other words in the singular,
+              plural, capitalization and/or he/she or they, are taken as
+              interchangeable and therefore as referring to the same.
               <PrivacySectionHeadings>Cookies</PrivacySectionHeadings>
               We employ the use of cookies. By accessing
-              https://www.tradex.live, you agreed to use cookies in agreement
-              with the TradeXlive Privacy Policy. Most interactive websites use
-              cookies to let us retrieve the user’s details for each visit.
-              Cookies are used by our website to enable the functionality of
-              certain areas to make it easier for people visiting our website.
-              Some of our affiliate/advertising partners may also use cookies.
+              https://www.shyamtraders.net, you agreed to use cookies in
+              agreement with the Shyam traders Privacy Policy. Most interactive
+              websites use cookies to let us retrieve the user’s details for
+              each visit. Cookies are used by our website to enable the
+              functionality of certain areas to make it easier for people
+              visiting our website. Some of our affiliate/advertising partners
+              may also use cookies.
               <PrivacySectionHeadings>Trading</PrivacySectionHeadings>
-              TradeX provides access to various financial markets and
+              Shyam Traders provides access to various financial markets and
               instruments, and you’re responsible for your trading decisions and
               actions. You must comply with all applicable laws and regulations
-              and use TradeX for lawful purposes only.
+              and use Shyam Traders for lawful purposes only.
               <PrivacySectionHeadings>Risk Disclosure</PrivacySectionHeadings>
               Trading involves risks, and you should carefully consider your
               investment objectives, risk tolerance, and financial situation
-              before using TradeX. You should also educate yourself about the
-              risks and benefits of trading and seek professional advice if
+              before using Shyam Traders. You should also educate yourself about
+              the risks and benefits of trading and seek professional advice if
               needed.
               <PrivacySectionHeadings>Fees and Charges</PrivacySectionHeadings>
-              TradeX may charge fees and commissions for its services, which may
-              vary depending on the type of account, financial instrument, and
-              market conditions. You’re responsible for paying all applicable
-              fees and charges.
+              Shyam Traders may charge fees and commissions for its services,
+              which may vary depending on the type of account, financial
+              instrument, and market conditions. You’re responsible for paying
+              all applicable fees and charges.
               <PrivacySectionHeadings>Termination</PrivacySectionHeadings>
-              TradeX may terminate or suspend your account at any time for any
-              reason, without prior notice. You may also terminate your account
-              at any time by following the instructions on the website.
+              Shyam Traders may terminate or suspend your account at any time
+              for any reason, without prior notice. You may also terminate your
+              account at any time by following the instructions on the website.
               <PrivacySectionHeadings>
                 Limitation of Liability
               </PrivacySectionHeadings>
-              TradeX is not liable for any direct or indirect damages, including
-              but not limited to loss of capital, data, or use, arising from
-              your use of TradeX or its services. TradeX also disclaims any
-              warranties, express or implied, including but not limited to
-              warranties of merchantability, fitness for a particular purpose,
-              and non-infringement.
+              Shyam Traders is not liable for any direct or indirect damages,
+              including but not limited to loss of capital, data, or use,
+              arising from your use of Shyam traders or its services. Shyam
+              traders also disclaims any warranties, express or implied,
+              including but not limited to warranties of merchantability,
+              fitness for a particular purpose, and non-infringement.
               <PrivacySectionHeadings>Terms of Trading</PrivacySectionHeadings>
               Line Trading (Chamki ) & Scalping is strictly prohibited at our
               platform. We reserve all the right to suspend the trade and
@@ -158,10 +144,10 @@ const PrivacyPolicy = () => {
               If a client does not use their account for 7 days, it will be
               locked for security reasons. To unlock the account, the client
               should contact customer support. These are the terms and
-              conditions outlined by Tradex, presumably for trading on their
-              platform. It’s important to read and understand these terms before
-              using their platform, as they outline the rules and expectations
-              for users. Here’s a breakdown of each point:
+              conditions outlined by Shyam traders, presumably for trading on
+              their platform. It’s important to read and understand these terms
+              before using their platform, as they outline the rules and
+              expectations for users. Here’s a breakdown of each point:
             </Content>
 
             <ServiceWrapper></ServiceWrapper>
